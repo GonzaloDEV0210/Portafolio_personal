8a69b07fb3bf442ec8ab2172db9c13d3718bccb3
Gonzalop0210 <gonzalopalmadev0210@gmail.com>
2023-05-23 21:50:01 -0500

    la barra de navegaión casi queda lista, corregir algunos errores que se encuenre

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const icon_bar = document.querySelector('.icon__bars');
+const ul_list = document.querySelector('.list__nav__bar');
+const icon_x = document.querySelector('.icon__x');
+
+function agregar_block() {
+    ul_list.classList.remove('mostrar__box');
+}
+
+function ocultar_block() {
+    ul_list.classList.add('mostrar__box');
+}
+
+icon_x.addEventListener('click', ocultar_block);
+icon_bar.addEventListener('click', agregar_block);
